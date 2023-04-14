91b271bbc7964b96128a29b98c8e66ab0dbf143b
Bobur <boburusmanov1994@gmail.com>
2023-04-14 12:41:01 +0500

    fix:"calendar"

--- src/containers/form/components/datepicker/index.jsx
+++ src/containers/form/components/datepicker/index.jsx
@@ -7,6 +7,7 @@ import {get, isEmpty, isFunction} from "lodash";
 import {ErrorMessage} from "@hookform/error-message";
 import {Calendar} from "react-feather";
 import dayjs from "dayjs";
+import ru from "date-fns/locale/ru"
 
 const Styled = styled.div`
   .custom-datepicker {
@@ -77,6 +78,8 @@ const CustomDatepicker = ({
                 {!get(property,'hideLabel',false) &&  <Label>{label ?? name}</Label>}
                 <div className={"custom__box"}>
                     <DatePicker
+                        locale={ru}
+                        calendarStartDay={1}
                         dateFormat={get(property,'dateFormat','dd.MM.yyyy')}
                         className={`custom-datepicker ${!isEmpty(errors) ? "error" : ''}`}
                         selected={dayjs(startDate).toDate()}
